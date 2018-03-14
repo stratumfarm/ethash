3b43f809dd029b339f196789c0c9c1ec4fa5a065
Korjavin Ivan <korjavin@gmail.com>
2018-03-14 22:29:46 +0500

    More logs

--- ethash.go
+++ ethash.go
@@ -157,11 +157,13 @@ func (l *Light) Verify(block Block) bool {
 	// Recompute the hash using the cache.
 	ok, mixDigest, result := cache.compute(uint64(dagSize), block.HashNoNonce(), block.Nonce())
 	if !ok {
+		log.Printf("compute !ok")
 		return false
 	}
 
 	// avoid mixdigest malleability as it's not included in a block's "hashNononce"
 	if block.MixDigest() != mixDigest {
+		log.Printf("Mix Digest diff %x %x", block.MixDigest(), mixDigest)
 		return false
 	}
 
